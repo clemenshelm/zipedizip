ff58ce81e23d2eaa3ac1175db968faa13fce4f23
Clemens Helm <clemens@chillbill.co>
2017-08-30 16:26:56 +0200

    Implement server-side examples of jszip.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const request = require('request-promise');
+const JSZip = require("jszip");
+const router = express.Router();
+const s3Url = 'https://s3.eu-central-1.amazonaws.com/zipedizip/'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Zipedizip' });
+});
+
+const logoFileNames = ['chillbill.png', 'meteor.png', 'react.png', 'viennajs.jpeg'];
+
+router.get('/zip-file.zip', function(req, res, next) {
+  const zip = new JSZip();
+
+  const zipPromises = logoFileNames.map(fileName =>
+    request({ url: `${s3Url}${fileName}`, encoding: null })
+      .then((content) => zip.file(fileName, content)));
+
+  Promise.all(zipPromises).then(() => {
+    zip.generateAsync({ type: 'nodebuffer' })
+      .then(zipContent => res.end(zipContent));
+  });
+});
+
+router.get('/zip-file-streamed.zip', function(req, res, next) {
+  const zip = new JSZip();
+
+  logoFileNames.forEach(fileName =>
+    zip.file(fileName, request({ url: `${s3Url}${fileName}`, encoding: null })));
+
+  zip.generateNodeStream({ streamFiles: true }).pipe(res);
 });
 
 module.exports = router;
